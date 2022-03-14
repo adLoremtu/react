ffbd60ca2d911773efab9d6d89a7600e260dbede
takuya inoue <takuya.inoue@nijibox.co.jp>
2022-03-15 04:20:21 +0900

    07_materialUI

--- react-product/02_chatbot-demo/chatbot-demo/src/components/answer.jsx
+++ react-product/02_chatbot-demo/chatbot-demo/src/components/answer.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import Button from '@mui/material/Button';
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles(() => ({
+    button: {
+        borderColor: "#ffb549",
+        color: '#ffb549',
+        fontWeight: 600,
+        marginBottom: '8px',
+        "&:hover": {
+            backgroundColor: '#ffb549',
+            borderColor: "#ffb549",
+            color: '#fff'
+        }
+    }
+}))
 
 const Answer = (props) => {
+    const classes = useStyles();
+
     return (
-        <Button variant="contained" onClick={() => props.select(props.content, props.nextId)}>
+        <Button
+            className={classes.button}
+            variant="outlined" onClick={() => props.select(props.content, props.nextId)}>
             {props.content}
         </Button>
     );
