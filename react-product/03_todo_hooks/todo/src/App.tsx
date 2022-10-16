67066177ff42ceb9cca34b80b7c0d42530988693
takuya-inoue <takuya.inoue@waku-2.com>
2022-10-16 15:22:54 +0900

    作業ファイルをコミット

--- react-product/03_todo_hooks/todo/src/App.tsx
+++ react-product/03_todo_hooks/todo/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './App.scss'
+import { ButtonAppBar } from './components/index'
 
 const App = () => {
     // 状態管理
@@ -7,6 +8,7 @@ const App = () => {
 
     return (
         <div>
+            <ButtonAppBar />
             <form onSubmit={(e) => e.preventDefault()}>
                 <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
                 <input type="submit" value="追加" onSubmit={(e) => e.preventDefault()} />
