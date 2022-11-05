47c53a4cf3440c6d2d730bc23cbe9be0aef0dff6
takuya-inoue <takuya.inoue@waku-2.com>
2022-11-05 13:34:37 +0900

    更新テスト

--- next_blog/nextjs-blog/pages/index.js
+++ next_blog/nextjs-blog/pages/index.js
@@ -24,7 +24,7 @@ export default function Home({ allPostsData }) {
         <p>[hoge fuga piyo]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <a href="https://nextjs.org/learn">our Next.js tutorial change test</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
